ca0768bbdab2f3a26282069deb313608da9b4ac7
codverch <mishradeepanjali25@gmail.com>
2023-04-30 20:14:18 +0530

    Fixed as_malloc() env-var

---
+++ gem5-source/as_malloc/env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set the environment variables
+export LD_LIBRARY_PATH=/home/deepanjali/deepanjali/Address-Scaling/gem5-source/as_malloc:$LD_LIBRARY_PATH
+export SCALING_FACTOR=2
+
+./test -n 3000 -e "b0" -t 1
